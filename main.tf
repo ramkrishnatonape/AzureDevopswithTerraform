eb7477db94803d02464f3fe729af5c1068b1fa63
Ram <ramkrishna@abd.com>
2022-01-28 15:54:32 +0530

    updated the main.tf file

--- main.tf
+++ main.tf
@@ -3,45 +3,31 @@ provider "azurerm" {
     features {}
 }
 
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-}
-
-variable "imagebuild" {
-  type        = string
-  description = "Latest Image Build"
-}
-
-
-
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
-  location = "Australia East"
+  location = "south india"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
+  name = "weatherapi"
+  location = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
 
-  ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
-  os_type             = "Linux"
+  ip_address_type = "public"
+  dns_name_label = "ramtonapewp"
+  os_type = "Linux"
 
   container {
-      name            = "weatherapi"
-      image           = "binarythistle/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
+    name = "weatherapi"
+    image = "ramtonape/weatherapi"
+      cpu = "1"
+      memory = "1"
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
+      ports {
+          port = 80
+          protocol = "TCP"
+      }
+
   }
+  
 }
